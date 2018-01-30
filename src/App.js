6ce1a5d5828524649c805cbbae17a90f7be0ac7d
Tevin Rivera <tevinrivera94@gmail.com>
2018-01-29 22:21:19 -0500

    Logging in and out work with firebase. COMPLETE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Card, CardSection, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDCOQGQrwDQAg-_8gma7an_SaJfTBsnnMQ',
@@ -15,12 +17,42 @@ class App extends Component {
       storageBucket: 'authentication-bbcc4.appspot.com',
       messagingSenderId: '1090548202061'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+      return (
+        <Card>
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        </Card>
+      );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner />;
+    }
+
+    return <LoginForm />;
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
